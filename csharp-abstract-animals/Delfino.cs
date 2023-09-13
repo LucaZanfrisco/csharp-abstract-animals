069da43c68c870ccb81af2da5dff8493a5ceacf1
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-13 15:14:50 +0200

    Classe Delfino

---
+++ csharp-abstract-animals/Delfino.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public class Delfino : Animale
+    {
+        public override void CosaMangia()
+        {
+            Console.WriteLine("pesce, crostacei e calamari");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("Fiu Fiu!!");
+        }
+    }
+}
