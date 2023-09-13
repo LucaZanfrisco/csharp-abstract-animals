061fe6399179ccf819b3a2c195d3edc2c66ca84f
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-13 15:12:48 +0200

    Classe Aquila

---
+++ csharp-abstract-animals/Aquila.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public class Aquila : Animale
+    {
+        public override void CosaMangia()
+        {
+            Console.WriteLine("volpi, lepri, marmotte, conigli selvatici e faine.");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("Graa graa!!");
+        }
+    }
+}
