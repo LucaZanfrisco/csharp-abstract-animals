03df3f3d950dcd1c2597c16be336b006b5d6a0e0
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-13 17:13:16 +0200

    refactor programma principale

--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -2,37 +2,153 @@
 
 using csharp_abstract_animals;
 
-Animale animale;
-IVolante volante;
-INuotante nuotante;
+Animale animale = null;
+IVolante volante = null;
+INuotante nuotante = null;
 
-volante = new Aquila();
-nuotante = new Delfino();
+bool selezioneOpzione = false;
 
-animale = new Passerotto();
-animale.CosaMangia();
-animale.Verso();
-animale.dormi();
-animale.FaiVolare(volante);
-animale.StampaZampe();
+while(!selezioneOpzione)
+{
+    Console.Write(@"1 - Cane;
+2 - Passerotto;
+3 - Delfino;
+4 - Aquila;
+Inserire il numero per selezionare:  ");
+    int numeroSelezionato = int.Parse(Console.ReadLine());
+    switch(numeroSelezionato)
+    {
+        case 1:
+            animale = new Cane();
+            selezioneOpzione = true;
+            break;
+        case 2:
+            animale = new Passerotto();
+            volante = new Passerotto();
+            selezioneOpzione = true;
+            break;
+        case 3:
+            animale = new Delfino();
+            nuotante = new Delfino();
+            selezioneOpzione = true;
+            break;
+        case 4:
+            animale = new Aquila();
+            volante = new Aquila();
+            selezioneOpzione = true;
+            break;
+        default:
+            Console.WriteLine("Il numero inserito non corrisponde a nulla. RIPROVARE!");
+            break;
+    }
+}
 
+bool continua = true;
+while(continua)
+{
+    int numeroSelezionato;
+    if(animale is Cane)
+    {
+        Console.Write(@"1 - Verso;
+2 - Cosa Mangia;
+3 - Numero Zampe;
+4 - Dormi
+Selezionare un numero per compiere un azione : ");
+        numeroSelezionato = int.Parse(Console.ReadLine());
+        switch(numeroSelezionato)
+        {
+            case 1:
+                animale.Verso();
+                break;
+            case 2:
+                animale.CosaMangia();
+                break;
+            case 3:
+                animale.StampaZampe();
+                break;
+            case 4:
+                animale.dormi();
+                break;
+            default:
+                Console.WriteLine("La scelta fatta non é valida");
+                break;
+        }
+    }
+    if(animale is Passerotto || animale is Aquila)
+    {
+        Console.Write(@"1 - Verso;
+2 - Cosa Mangia;
+3 - Numero Zampe;
+4 - Dormi
+5 - Vola
+6 - Numero Ali
+Selezionare un numero per compiere un azione : ");
+        numeroSelezionato = int.Parse(Console.ReadLine());
+        switch(numeroSelezionato)
+        {
+            case 1:
+                animale.Verso();
+                break;
+            case 2:
+                animale.CosaMangia();
+                break;
+            case 3:
+                animale.StampaZampe();
+                break;
+            case 4:
+                animale.dormi();
+                break;
+            case 5:
+                animale.FaiVolare(volante);
+                break;
+            case 6:
+                Console.WriteLine(volante.NumeroAli() + " ali");
+                break;
+            default:
+                Console.WriteLine("La scelta fatta non é valida");
+                break;
+        }
+    }
+    if(animale is Delfino)
+    {
+        Console.Write(@"1 - Verso;
+2 - Cosa Mangia;
+3 - Numero Zampe;
+4 - Dormi
+5 - Nuota
+6 - Numero Pinne
+Selezionare un numero per compiere un azione : ");
+        numeroSelezionato = int.Parse(Console.ReadLine());
+        switch(numeroSelezionato)
+        {
+            case 1:
+                animale.Verso();
+                break;
+            case 2:
+                animale.CosaMangia();
+                break;
+            case 3:
+                animale.StampaZampe();
+                break;
+            case 4:
+                animale.dormi();
+                break;
+            case 5:
+                animale.FaiNuotare(nuotante);
+                break;
+            case 6:
+                Console.WriteLine(nuotante.NumeroPinne() + " pinne");
+                break;
+            default:
+                Console.WriteLine("La scelta fatta non é valida");
+                break;
+        }
+    }
+    Console.Write("Se si desidera ripetere l'esecuzione scrivere Y/N : ");
+    string rispostaUtente = Console.ReadLine();
+    if(rispostaUtente.ToLower() == "n")
+    {
+        continua = false;
+    }
 
-animale = new Cane();
-animale.CosaMangia();
-animale.Verso();
-animale.dormi();
-animale.StampaZampe();
-
-
-animale = new Delfino();
-animale.CosaMangia();
-animale.Verso();
-animale.dormi();
-animale.FaiNuotare(nuotante);
-animale.StampaZampe();
-
-animale = new Aquila();
-animale.CosaMangia();
-animale.Verso();
-animale.dormi();
-animale.StampaZampe();
+}
