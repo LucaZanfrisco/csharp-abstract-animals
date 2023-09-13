3456c9f40c1d161094bf87b4daf3b0aebf3a453b
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-13 15:05:34 +0200

    Creazione della classe astratta animale con reletivi metodi

---
+++ csharp-abstract-animals/Animale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public abstract class Animale
+    {
+
+        public void dormi()
+        {
+            Console.WriteLine("Zzzzzzz.....");
+        }
+        public abstract void Verso();
+
+        public abstract void CosaMangia();
+
+    }
+}
